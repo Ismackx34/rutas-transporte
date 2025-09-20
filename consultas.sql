11b44fc6319e636b06a89008dd0222920b3959a9
Ismackx34 <jair1002.rivera@gmail.com>
2025-09-19 22:03:33 -0500

    subida, consultas, actualizadas

--- consultas.sql
+++ consultas.sql
@@ -5,6 +5,24 @@ Para el día específico '2025-09-11', lo que hace es agrupar todos los viajes i
 de su horario (H.hora_inicio, H.hora_fin, H.unidades_en_servicio). Luego, cuenta cuántos viajes
 (usuarios individuales) caen en cada uno de estos grupos.
 */
+
+-- creacion de indices 
+CREATE INDEX idx_uso_fecha_ruta_horario 
+ON Uso_Transporte_NEW(fecha, id_ruta, id_horario);
+
+CREATE INDEX idx_uso_fecha_parada_abordaje 
+ON Uso_Transporte_NEW(fecha, id_parada_abordaje);
+
+CREATE INDEX idx_uso_fecha_parada_descenso 
+ON Uso_Transporte_NEW(fecha, id_parada_descenso);
+
+-- Índice para búsquedas rápidas (reportes y agrupaciones)
+CREATE INDEX idx_paradas_nombre_direccion 
+ON Paradas(nombre_parada, direccion);
+
+-- Índice útil para joins frecuentes
+CREATE INDEX idx_horarios_ruta ON Horarios(id_ruta);
+
 SELECT R.nombre_ruta, H.hora_inicio, H.hora_fin, H.unidades_en_servicio, 
 COUNT(UT.id_uso) AS numero_usuarios_en_horario_y_ruta
 FROM Uso_Transporte_NEW UT 
@@ -17,12 +35,17 @@ ORDER BY numero_usuarios_en_horario_y_ruta DESC;
 
 
 
+
+
 -- 4. Paradas donde más y menos usuarios abordan  (se suben)
 /*
 Esta consulta identifica las 10 paradas desde donde un mayor número de usuarios inician sus viajes
 en el día '2025-09-11'. Esto es clave para identificar los puntos de origen más populares,
 evaluar la infraestructura de esas paradas o planificar servicios adicionales.
 */
+
+
+
 SELECT P.nombre_parada, P.direccion, COUNT(UT.id_uso) AS total_usuarios_abordajes
 FROM Uso_Transporte_NEW UT
 JOIN Paradas P ON UT.id_parada_abordaje = P.id_parada
@@ -44,6 +67,7 @@ Esta consulta identifica las 10 paradas donde un mayor número de usuarios final
 en el día '2025-09-11'. Esto es esencial para detectar los principales destinos de los usuarios,
 lo cual puede informar sobre la planificación urbana, centros de actividad o puntos de transbordo críticos.
 */
+
 SELECT P.nombre_parada, P.direccion, COUNT(UT.id_uso) AS total_usuarios_descensos
 FROM Uso_Transporte_NEW UT
 JOIN Paradas P ON UT.id_parada_descenso = P.id_parada
@@ -67,6 +91,9 @@ Esto es crucial para entender cómo los usuarios navegan la red, permitiendo opt
 segmentos de rutas, considerar servicios directos entre puntos clave o reajustar
 capacidades donde los flujos de pasajeros son más intensos.
 */
+
+
+
 SELECT R.nombre_ruta,
     P_origen.nombre_parada AS parada_origen,
     P_destino.nombre_parada AS parada_destino,
@@ -80,5 +107,3 @@ GROUP BY R.nombre_ruta, P_origen.nombre_parada, P_destino.nombre_parada
 ORDER BY numero_usuarios_viaje_od DESC;
 
 select * from usuarios;
-
-
